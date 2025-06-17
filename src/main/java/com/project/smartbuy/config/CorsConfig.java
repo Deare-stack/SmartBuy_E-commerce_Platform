6e67d7f3439edf5270fb7e4d81b62e1f0e9de490
Deare <deare121@gmail.com>
2025-06-16 23:15:20 -0400

    🔒🔒 feat: Only allow CORS requests from React dev server (http://localhost:3000)

--- src/main/java/com/project/smartbuy/config/CorsConfig.java
+++ src/main/java/com/project/smartbuy/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Allow all paths
-                        .allowedOriginPatterns("http://localhost:5173") // Allow all front-end
+                        .allowedOriginPatterns("http://localhost:3000") // Allow all front-end
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
